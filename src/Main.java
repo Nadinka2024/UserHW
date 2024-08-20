f89c6cff1c7ed4d21524cf745777be8d75dd4f6e
nadezhdachurilo <nadezhdachurilo@gmail.com>
2024-08-20 23:13:04 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import lesson3.User;
+
+    public class Main {
+        public static void main(String[] args) {
+            User zeroUser = new User( "Vitold", "vitold@gmail.com");
+            User secondUser = new User ("Bonie", "bonie@gmail.com");
+
+            zeroUser.addFollower();
+            zeroUser.addFollower();
+            secondUser.addFollower();
+            zeroUser.addPost();
+            zeroUser.addPost();
+            secondUser.addComment();
+            secondUser.addComment();
+
+            System.out.println("Common count of followers" + User.countOfFollowerForAllUsers);
+            zeroUser.printDetails();
+            secondUser.printDetails();
+        }
+}
---
+++ src/lesson3/User.java
@@ -0,0 +1,37 @@
+package lesson3;
+
+public class User {
+    public static int countOfFollowerForAllUsers;
+    public static int postOfFollowerForAllUser;
+    public static int commentOfFollowerAllUser;
+    public String userName;
+    public String mail;
+    public int countOfFollowers;
+
+    public User(String userName, String mail){
+        this.userName = userName;
+        this.mail = mail;
+    }
+
+    public int addFollower(){
+        return ++countOfFollowers;
+    }
+
+    public int addPost(){
+        return ++postOfFollowerForAllUser;
+    }
+
+    public int addComment(){
+        return ++commentOfFollowerAllUser;
+    }
+
+    public void printDetails(){
+        System.out.println("User full name: " +userName);
+        System.out.println("Email:" + mail);
+        System.out.println("Count of followers: " + countOfFollowers);
+        System.out.println("Post of follower: " + postOfFollowerForAllUser);
+        System.out.println("Comment of follower: " + commentOfFollowerAllUser);
+    }
+}
+
+
